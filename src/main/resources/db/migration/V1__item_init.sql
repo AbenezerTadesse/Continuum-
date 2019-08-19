e6c693bcc56d3d795223e992e9d5a0c9ca7ec675
Abex47 <abenezerdjn@gmail.com>
2019-08-18 18:22:01 -0600

    Seed data script added.

--- src/main/resources/db/migration/V1__item_init.sql
+++ src/main/resources/db/migration/V1__item_init.sql
@@ -5,3 +5,14 @@ CREATE TABLE ITEM(
     PRIMARY KEY (ITEM_ID),
     FOREIGN KEY (SUB_ITEM_OF_ID) REFERENCES (ITEM_ID)
 );
+
+INSERT INTO ITEM (ITEM_ID, ITEM_NAME, SUB_ITEM_OF_ID) VALUES
+  ('1', 'Item1', null),
+  ('2', 'Item2', null),
+  ('3', 'Item3', null),
+  ('4', 'Item10', '1'),
+  ('5', 'Item11', '1'),  ('6', 'Item12', '1'),
+  ('7', 'Item100', '4'),  ('8', 'Item101', '4'),
+  ('9', 'Item102', '4'),  ('10', 'Item30', '3'),
+  ('11', 'Item31', '3'),  ('12', 'Item32', '3')
+  ;

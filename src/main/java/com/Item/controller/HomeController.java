6f76193c26e84721f10f9a9fb86863390f6ea5f0
Abex47 <abenezerdjn@gmail.com>
2019-08-18 18:07:38 -0600

    Initial Spring Boot setup

---
+++ src/main/java/com/Item/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.Item.controller;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class HomeController {
+    @RequestMapping("/")
+    public String home(){
+        return "Continuum Take Home Project By Abenezer Tadesse";
+    }
+}
